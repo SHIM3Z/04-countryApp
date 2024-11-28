f0017d11d8466701c1960a1a1aea053717bebe2c
Paolo Siancas <shim3z151002@gmail.com>
2024-11-28 12:41:43 -0500

    fin de la seccion 9

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -9,6 +10,8 @@ import { switchMap } from 'rxjs';
   styleUrl: './country-page.component.css',
 })
 export class CountryPageComponent implements OnInit {
+  public country?: Country;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private countryService: CountriesService,
@@ -24,7 +27,7 @@ export class CountryPageComponent implements OnInit {
           this.router.navigateByUrl('');
           return;
         }
-        console.log('Tenemos un país');
+        this.country = country;
       });
   }
 }
